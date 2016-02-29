524d9c065076c7cd9fad81c1111f18339cc5102c
Kenson Man <kenson@breakthrough.org.hk>
2016-02-29 15:30:37 +0800

    Fixing the framework layout and add the getitem templatetag

--- providers.py
+++ providers.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 
-
 def absolute_path(req):
 	'''
 	Get the absolute-path of the application or request
@@ -11,4 +10,15 @@ def absolute_path(req):
 		RST[ATTR]=getattr(settings, ATTR)
 	else:
 		RST[ATTR]=req.get_host()
+	return RST
+
+def template_injection(req):
+	'''
+	Inject the TMPL_* settings into the request
+	'''
+	RST={
+		'TMPL_BASE': getattr(settings, 'TMPL_BASE', 'webframe/framework.html'),
+		'TMPL_HEADER': getattr(settings, 'TMPL_HEADER', 'webframe/header.html'),
+		'TMPL_FOOTER': getattr(settings, 'TMPL_FOOTER', 'webframe/footer.html'),
+	}
 	return RST
