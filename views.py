f8850e3140b5f0ac9ba673c02157e9835737a367
Kenson Man <kenson@breakthrough.org.hk>
2016-10-08 08:47:07 +0800

    Draft the preferences page

--- views.py
+++ views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404 as getObj
+from django.contrib.auth.decorators import login_required
+from webframe.models import *
 
-# Create your views here.
+@login_required
+def prefs(req, user=None):
+	'''
+	Show the preference page of the specified user. 
+	If requesting to show user that is not current session,
+	the superuser permission are required.
+	'''
+	if user==None: user=req.user.username
+	if not req.user.is_superuser:
+		if req.user.username!=user: return HttpResponseForbidden()
+	params=dict()
+	params['target']=Preference.objects.filter(user=req.user)
+	params['general']=Preference.objects.filter(user__isnull=True)
+	return render(req, 'webframe/preferences.html', params)
+
+@login_required
+def pref(req, user=None, prefId=None):
+	if prefId=='add':
+		pref=Preference()
+	else:
+		pref=getObj(Preference, id=prefId)
+	params=dict()
+	params['target']=pref
+	return render(req, 'webframe/preference.html', params)
