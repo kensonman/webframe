63027c20adf379466f8e02955384f1d677aaf479
Kenson Man <kenson@mansonsolutions.hk>
2016-10-10 20:43:08 +0800

    Upgrade the ValueObject and AliveObject

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone as tz
 from django.utils.translation import ugettext_lazy as _
 from .CurrentUserMiddleware import get_current_user
 import math, uuid
@@ -9,10 +10,43 @@ class ValueObject(models.Model):
 		abstract		= True
 		verbose_name		= _('ValueObject')
 		verbose_name_plural	= _('ValueObjects')
+		permissions		= ( ('view_%(class)s', 'Can view this model without ownership'), )
 
-	enabled			= models.BooleanField(default=True, verbose_name=_('ValueObject.enabled'),help_text=_('ValueObject.enabled.helptext'))
-	lmb			= models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True,related_name='%(class)s_lmd',verbose_name=_('ValueObject.lmb'),help_text=_('ValueObject.lmb.helptext'))
-	lmd			= models.DateTimeField(auto_now=True, verbose_name=_('ValueObject.lmd'),help_text=_('ValueObject.lmd.helptext'))
+	id					= models.UUIDField(
+							primary_key=True,
+							default=uuid.uuid4,
+							editable=False,
+							verbose_name=_('ValueObject.id'),
+							help_text=_('ValueObject.id.helptext'),
+						)
+	lmd					= models.DateTimeField(
+							auto_now=True,
+							verbose_name=_('ValueObject.lmd'),
+							help_text=_('ValueObject.lmd.helptext'),
+						)
+	lmb					= models.ForeignKey(
+							settings.AUTH_USER_MODEL,
+							default=get_current_user,
+							null=True,
+							blank=True,
+							related_name='%(class)s_lmb',
+							verbose_name=_('ValueObject.lmb'),
+							help_text=_('ValueObject.lmb.helptext'),
+						)
+	cd					= models.DateTimeField(
+							auto_now_add=True,
+							verbose_name=_('ValueObject.cd'),
+							help_text=_('ValueObject.cd.helptext'),
+						)
+	cb					= models.ForeignKey(
+							settings.AUTH_USER_MODEL,
+							default=get_current_user,
+							null=True,
+							blank=True,
+							related_name='%(class)s_cb',
+							verbose_name=_('ValueObject.cb'),
+							help_text=_('ValueObject.cb.helptext'),
+						)
 
 	def isNew(self):
 		return self.lmd is None
@@ -24,7 +58,68 @@ class ValueObject(models.Model):
 		user=get_current_user()
 		if not (user or user.is_authenticated()): user=None
 		self.lmb=user
+		if not self.cb: self.cb=user
 		super(ValueObject, self).save()
+
+class AliveObjectManager(models.Manager):
+	def living(self):
+		now=tz.now()
+		return self.filter(enabled=True,effDate__lte=now).filter(models.Q(expDate__isnull=True)|models.Q(expDate__gt=now)).order_by('-effDate')
+
+class AliveObject(models.Model):
+	class Meta(object):
+		abstract			= True
+		verbose_name		= _('AliveObject')
+		verbose_name_plural	= _('AliveObjects')
+	
+	effDate					= models.DateTimeField(
+							default=tz.now,
+							verbose_name=_('AliveObject.effDate'),
+							help_text=_('AliveObject.effDate.helptext'),
+						)
+	expDate					= models.DateTimeField(
+							null=True,
+							blank=True,
+							verbose_name=_('AliveObject.expDate'),
+							help_text=_('AliveObject.expDate.helptext'),
+						)
+	enabled					= models.BooleanField(
+							default=True,
+							verbose_name=_('AliveObject.enabled'),
+							help_text=_('AliveObject.enabled.helptext'),
+						)
+
+	def isNew(self):
+		'''
+		Determinate the object is just create (haven't saved).
+		'''
+		return self.lmd is None
+
+	def isNotNew(self):
+		'''
+		Determinate the object is create create (haven't saved).
+		'''
+		return not self.lmd is None
+
+	def isEffective(self, now=None):
+		'''
+		Deprecated. Use isAlive(now=None) instead.
+		'''
+		return self.isAlive(now)
+
+	def isAlive(self, now=None):
+		'''
+		Determinate the objct is alive (is-effective).
+		'''
+		if not now: now=tz.now()
+		return self.enabled and self.effDate<=now and (self.expDate is None or self.expDate>now)
+
+	def isDead(self, now=None):
+		'''
+		Determinate the objct was dead (is-not-effective).
+		'''
+		return not self.isAlive(now)
+		
 
 class PrefManager(models.Manager):
 	def pref(self, name, **kwargs):
@@ -44,7 +139,6 @@ class Preference(ValueObject):
 			('browse_config', 'Can browse system configuration'),
 			('browse_preference', 'Can browse other preferences'),
 		)
-	id                                      = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False,verbose_name=_('Preference.id'),help_text=_('Preference.id.helptext'))
 	name					= models.CharField(max_length=100,verbose_name=_('Preference.name'),help_text=_('Preference.name.helptext'))
 	value					= models.CharField(max_length=1024,verbose_name=_('Preference.value'),help_text=_('Preference.value.helptext'))
 	owner					= models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True,related_name='preference_owner',verbose_name=_('Preference.owner'),help_text=_('Preference.owner.helptext'))
