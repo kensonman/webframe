7a78525bb8cc6176dfbe348e5f95373c1d70628f
Kenson Man <kenson@mansonsolutions.hk>
2016-12-02 18:36:51 +0800

    Add the checkRecaptcha( req, secret, simple=True ) function

--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@
 def getClientIP( req ):
 	'''
 	Get the client ip address
+
+	@param req The request;
 	'''
 	xForwardedFor=req.META.get('HTTP_X_FORWARDED_FOR')
 	if xForwardedFor:
@@ -11,10 +13,34 @@ def getClientIP( req ):
 		ip=req.META.get('REMOTE_ADDR')
 	return ip
 
-def getBool( val, trueOpts=['YES', 'Y', '1', 'TRUE', 'T'] ):
+def getBool( val, defVal=False, trueOpts=['YES', 'Y', '1', 'TRUE', 'T'] ):
 	'''
 	Retrieve the boolean value from string
+
+	@param val The value to be parse to bool
+	@param defVal The default value if the val is None
+	@param trueOpts The available values of TRUE
 	'''
 	if val:
 		return str(val).upper() in trueOpts
-	return False
+	return defVal 
+
+def checkRecaptcha( req, secret, simple=True ):
+	'''
+	Checking the recaptcha and return the result.
+
+	@param req The request;
+	@param secret The secret retreived from Google reCaptcha registration;
+	@param simple Retrue the simple boolean value of verification if True, otherwise, return the JSON value of verification;
+	'''
+	import requests
+	apiurl='https://www.google.com/recaptcha/api/siteverify'
+	fieldname='g-recaptcha-response'
+
+	answer=req.POST.get(fieldname, None)
+	clientIP=getClientIP( req )
+	rst=requests.post(apiurl, data={'secret': secret, 'response':answer, 'remoteip': clientIP}).json()
+	if simple:
+		return getBool(rst.get('success', 'False'))
+	return r.json()
+	
