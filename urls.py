fd11e57f736fff6ef23972dee642554c6e8f5495
Kenson Man <kenson@breakthrough.org.hk>
2017-05-11 18:05:49 +0800

    Add the prefs and pref url

--- urls.py
+++ urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
 	url(r'^login/$', views.login, name='login'),
 	url(r'^logout/$', views.logout, name='logout'),
 	url(r'^users/$', views.users, name='users'),
-	url(r'^users/(?P<username>[^/]*)/$', views.user, name='user'),
+	url(r'^users/(?P<user>[^/]*)/$', views.user, name='user'),
+	url(r'^users/(?P<user>[^/]*)/prefs/$', views.prefs, name='prefs'),
+	url(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/$', views.pref, name='pref'),
 ] 
