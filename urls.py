32c19721e87e6f7dda4c61ee9432d0ff7f4d59af
Kenson Man <kenson@mansonsolutions.hk>
2017-05-20 13:32:19 +0800

    Fixing the tab error

--- urls.py
+++ urls.py
@@ -3,10 +3,10 @@ from . import views
 from django.conf import settings
 
 urlpatterns=[
-	url(r'^login/$', views.login, name='login'),
-	url(r'^logout/$', views.logout, name='logout'),
-	url(r'^users/$', views.users, name='users'),
-	url(r'^users/(?P<user>[^/]*)/$', views.user, name='user'),
-	url(r'^users/(?P<user>[^/]*)/prefs/$', views.prefs, name='prefs'),
-	url(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/$', views.pref, name='pref'),
+	url(r'^login/?$', views.login, name='login'),
+	url(r'^logout/?$', views.logout, name='logout'),
+	url(r'^users/?$', views.users, name='users'),
+	url(r'^users/(?P<user>[^/]*)/?$', views.user, name='user'),
+	url(r'^users/(?P<user>[^/]*)/prefs/?$', views.prefs, name='prefs'),
+	url(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/?$', views.pref, name='pref'),
 ] 
--- views.py
+++ views.py
@@ -45,186 +45,186 @@ def logout(req):
 
 @login_required
 def users(req):
-	'''
-	Show the users page. It is supported to the default User Model (django.contrib.auth.models.User)
-	'''
-	# Check permission
-	if req.user.is_superuser:
-		pass
-	elif req.user.has_perms('auth.browse_user'):
-		pass
-	else:
-		return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
+    '''
+    Show the users page. It is supported to the default User Model (django.contrib.auth.models.User)
+    '''
+    # Check permission
+    if req.user.is_superuser:
+        pass
+    elif req.user.has_perms('auth.browse_user'):
+        pass
+    else:
+        return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
 
-	params=dict()
-	params['target']=UserTable(get_user_model().objects.all())
-	rc=RequestConfig(req)
-	rc.configure(params['target'])
-	return render(req, getattr(settings, 'TMPL_USERS', 'webframe/users.html'), params)
+    params=dict()
+    params['target']=UserTable(get_user_model().objects.all())
+    rc=RequestConfig(req)
+    rc.configure(params['target'])
+    return render(req, getattr(settings, 'TMPL_USERS', 'webframe/users.html'), params)
 
 @login_required
 def user(req, user):
-	user=get_user_model()() if user=='add' or user=='new' else getObj(get_user_model(), username=user)
-	params=dict()
-	args=QueryDict(req.body)
+    user=get_user_model()() if user=='add' or user=='new' else getObj(get_user_model(), username=user)
+    params=dict()
+    args=QueryDict(req.body)
 
-	if req.method=='GET':
-		# Check permission
-		if req.user.is_superuser:
-			pass
-		elif req.user.username==username:
-			pass
-		elif req.user.has_perms('auth.browse_user'):
-			pass
-		else:
-			return HttpResponseForbidden()
+    if req.method=='GET':
+        # Check permission
+        if req.user.is_superuser:
+            pass
+        elif req.user.username==username:
+            pass
+        elif req.user.has_perms('auth.browse_user'):
+            pass
+        else:
+            return HttpResponseForbidden()
 
-		# Generate the result
-		params['target']=user
-		return render(req, getattr(settings, 'TMPL_USER', 'webframe/user.html'), params)
-	elif req.method=='DELETE':
-		_('User.msg.confirmDelete')
-		user.delete()
-		return redirect(args.get('next', 'users'))
-	elif req.method=='POST':
-		# Check permission
-		if req.user.is_superuser:
-			pass
-		elif req.user==user:
-			pass
-		elif user.id is None and req.user.has_perms('auth.add_user'):
-			pass
-		elif user.id is not None and req.user.has_perms('auth.change_user'):
-			pass
-		else:
-			return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
+        # Generate the result
+        params['target']=user
+        return render(req, getattr(settings, 'TMPL_USER', 'webframe/user.html'), params)
+    elif req.method=='DELETE':
+        _('User.msg.confirmDelete')
+        user.delete()
+        return redirect(args.get('next', 'users'))
+    elif req.method=='POST':
+        # Check permission
+        if req.user.is_superuser:
+            pass
+        elif req.user==user:
+            pass
+        elif user.id is None and req.user.has_perms('auth.add_user'):
+            pass
+        elif user.id is not None and req.user.has_perms('auth.change_user'):
+            pass
+        else:
+            return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
 
-		user.first_name=req.POST.get('first_name', None)
-		user.last_name=req.POST.get('last_name', None)
-		user.email=req.POST.get('email', None)
-		if req.user.is_superuser or req.user.has_perms('auth.add_user') or req.user.has_perms('auth.change_user'):
-			user.username=req.POST.get('username', user.username)
-			user.is_superuser = req.POST.get('is_superuser', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
-			user.is_active = req.POST.get('is_active', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
-			user.is_staff = req.POST.get('is_staff', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
+        user.first_name=req.POST.get('first_name', None)
+        user.last_name=req.POST.get('last_name', None)
+        user.email=req.POST.get('email', None)
+        if req.user.is_superuser or req.user.has_perms('auth.add_user') or req.user.has_perms('auth.change_user'):
+            user.username=req.POST.get('username', user.username)
+            user.is_superuser = req.POST.get('is_superuser', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
+            user.is_active = req.POST.get('is_active', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
+            user.is_staff = req.POST.get('is_staff', '').upper() in ['TRUE', 'T', 'YES', 'Y', '1']
         password=req.POST.get('password', None)
         if password:
             user.set_password(password)
-		user.save()
-		return redirect(args.get('next', 'users'))
-	elif req.method=='PUT': #The PUT method is used for user to update their own personal information, webframe/preferences.html
-		# Check permission
-		if req.user.is_superuser:
-			pass
-		elif req.user==user:
-			pass
-		elif user.id is not None and req.user.has_perms('auth.change_user'):
-			pass
-		else:
-			return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
+        user.save()
+        return redirect(args.get('next', 'users'))
+    elif req.method=='PUT': #The PUT method is used for user to update their own personal information, webframe/preferences.html
+        # Check permission
+        if req.user.is_superuser:
+            pass
+        elif req.user==user:
+            pass
+        elif user.id is not None and req.user.has_perms('auth.change_user'):
+            pass
+        else:
+            return HttpResponseForbidden('<h1>403 - Forbidden</h1>')
 
-		user.first_name=req.POST.get('first_name', None)
-		user.last_name=req.POST.get('last_name', None)
-		user.email=req.POST.get('email', None)
-		user.save()
-		return redirect(args.get('next', 'users'))
+        user.first_name=req.POST.get('first_name', None)
+        user.last_name=req.POST.get('last_name', None)
+        user.email=req.POST.get('email', None)
+        user.save()
+        return redirect(args.get('next', 'users'))
 
 @login_required
 def prefs(req, user=None):
-	'''
-	Show the preference page of the specified user. 
-	If requesting to show user that is not current session,
-	the superuser permission are required.
-	'''
-	if user==None: user=req.user.username
-	if user!=req.user.username and not req.user.is_superuser:
-		if req.user.username!=user: return HttpResponseForbidden()
-	user=getObj(get_user_model(), username=user)
-	params=dict()
-	params['preference']=PreferenceTable(Preference.objects.filter(owner=req.user, parent__isnull=True))
-	params['config']=PreferenceTable(Preference.objects.filter(owner__isnull=True, parent__isnull=True))
-	rc=RequestConfig(req)
-	rc.configure(params['preference'])
-	rc.configure(params['config'])
-	params['currentuser']=user
-	if req.user.has_perms('webframe.add_config') or req.user.has_perms('webframe.change.config'):
-		m=hashlib.md5()
-		m.update(user.username.encode('utf-8'))
-		m.update(CONFIG_KEY.encode('utf-8'))
-		params['config_key']=m.hexdigest()
-	return render(req, getattr(settings, 'TMPL_PREFERENCES', 'webframe/preferences.html'), params)
+    '''
+    Show the preference page of the specified user. 
+    If requesting to show user that is not current session,
+    the superuser permission are required.
+    '''
+    if user==None: user=req.user.username
+    if user!=req.user.username and not req.user.is_superuser:
+        if req.user.username!=user: return HttpResponseForbidden()
+    user=getObj(get_user_model(), username=user)
+    params=dict()
+    params['preference']=PreferenceTable(Preference.objects.filter(owner=req.user, parent__isnull=True))
+    params['config']=PreferenceTable(Preference.objects.filter(owner__isnull=True, parent__isnull=True))
+    rc=RequestConfig(req)
+    rc.configure(params['preference'])
+    rc.configure(params['config'])
+    params['currentuser']=user
+    if req.user.has_perms('webframe.add_config') or req.user.has_perms('webframe.change.config'):
+        m=hashlib.md5()
+        m.update(user.username.encode('utf-8'))
+        m.update(CONFIG_KEY.encode('utf-8'))
+        params['config_key']=m.hexdigest()
+    return render(req, getattr(settings, 'TMPL_PREFERENCES', 'webframe/preferences.html'), params)
 
 @login_required
 def pref(req, user=None, prefId=None):
-	'''
-	Showing the preference form for input.
-	'''
-	# Declare the preference's owner
-	if user==None or user=='None': user=req.user.username
-	if user!=req.user.username and not req.user.is_superuser:
-		if req.user.username!=user: return HttpResponseForbidden()
-	user=getObj(get_user_model(), username=user)
-	params=dict()
-	m=hashlib.md5()
-	m.update(user.username.encode('utf-8'))
-	m.update(CONFIG_KEY.encode('utf-8'))
-	params['config_key']=m.hexdigest()
+    '''
+    Showing the preference form for input.
+    '''
+    # Declare the preference's owner
+    if user==None or user=='None': user=req.user.username
+    if user!=req.user.username and not req.user.is_superuser:
+        if req.user.username!=user: return HttpResponseForbidden()
+    user=getObj(get_user_model(), username=user)
+    params=dict()
+    m=hashlib.md5()
+    m.update(user.username.encode('utf-8'))
+    m.update(CONFIG_KEY.encode('utf-8'))
+    params['config_key']=m.hexdigest()
 
-	# Get the target preference
-	if prefId=='add' or prefId=='new':
-		pref=Preference()
-		pref.owner=user
-		if 'config_key' in req.GET:
-			if params['config_key']==req.GET['config_key']:
-				pref.owner=None
-				params['config_key']=m.hexdigest()
-		if 'parent' in req.GET: pref.parent=getObj(Preference, id=req.GET['parent'])
-	else:
-		pref=getObj(Preference, id=prefId)
-	
-	if req.method=='GET':
-		# Preparing the form view
-		params['target']=pref
-		params['childs']=PreferenceTable(pref.childs())
-		params['currentuser']=user
-		return render(req, getattr(settings, 'TMPL_PREFERENCE', 'webframe/preference.html'), params)
-	elif req.method=='POST':
-		# Security Checking
-		if req.POST.get('user', None):
-			if pref.isNew() and req.user.has_perms('webframe.add_preference'):
-				pass #Allow to add preference
-			elif (not pref.isNew()) and req.user.has_perms('webframe.change_preference'):
-				pass #Allow to change preference
-			else:
-				return HttpResponseForbidden()
-			pref.owner=getObj(get_user_model(), username=req.POST['user'])
-		else:
-			if pref.isNew() and req.user.has_perms('webframe.add_config'):
-				pass #Allow to add config 
-			elif (not pref.isNew()) and req.user.has_perms('webframe.change_config'):
-				pass #Allow to change config 
-			else:
-				return HttpResponseForbidden()
-			m=hashlib.md5()
-			m.update(user.username.encode('utf-8'))
-			m.update(CONFIG_KEY.encode('utf-8'))
-			if m.hexdigest()!=req.POST.get('config_key', None):
-				print('Invalid config_key')
-				return HttpResponseForbidden()
-			pref.owner=None
+    # Get the target preference
+    if prefId=='add' or prefId=='new':
+        pref=Preference()
+        pref.owner=user
+        if 'config_key' in req.GET:
+            if params['config_key']==req.GET['config_key']:
+                pref.owner=None
+                params['config_key']=m.hexdigest()
+        if 'parent' in req.GET: pref.parent=getObj(Preference, id=req.GET['parent'])
+    else:
+        pref=getObj(Preference, id=prefId)
+    
+    if req.method=='GET':
+        # Preparing the form view
+        params['target']=pref
+        params['childs']=PreferenceTable(pref.childs())
+        params['currentuser']=user
+        return render(req, getattr(settings, 'TMPL_PREFERENCE', 'webframe/preference.html'), params)
+    elif req.method=='POST':
+        # Security Checking
+        if req.POST.get('user', None):
+            if pref.isNew() and req.user.has_perms('webframe.add_preference'):
+                pass #Allow to add preference
+            elif (not pref.isNew()) and req.user.has_perms('webframe.change_preference'):
+                pass #Allow to change preference
+            else:
+                return HttpResponseForbidden()
+            pref.owner=getObj(get_user_model(), username=req.POST['user'])
+        else:
+            if pref.isNew() and req.user.has_perms('webframe.add_config'):
+                pass #Allow to add config 
+            elif (not pref.isNew()) and req.user.has_perms('webframe.change_config'):
+                pass #Allow to change config 
+            else:
+                return HttpResponseForbidden()
+            m=hashlib.md5()
+            m.update(user.username.encode('utf-8'))
+            m.update(CONFIG_KEY.encode('utf-8'))
+            if m.hexdigest()!=req.POST.get('config_key', None):
+                print('Invalid config_key')
+                return HttpResponseForbidden()
+            pref.owner=None
 
-		# Saving
-		pref.name=req.POST['name']
-		pref.value=req.POST['value']
-		pref.sequence=int(req.POST['sequence'])
-		pref.parent=None if req.POST.get('parent') is None else getObj(Preference, id=req.POST['parent'])
-		pref.save()
-	elif req.method=='DELETE':
-		# Delete the method
-		_('Preference.msg.confirmDelete')
-		pref.delete()
-	if pref.parent:
-		return redirect('preference', user=user.username, prefId=pref.parent.id)
-	else:
-		return redirect('preferences', user=user.username)
-		
+        # Saving
+        pref.name=req.POST['name']
+        pref.value=req.POST['value']
+        pref.sequence=int(req.POST['sequence'])
+        pref.parent=None if req.POST.get('parent') is None else getObj(Preference, id=req.POST['parent'])
+        pref.save()
+    elif req.method=='DELETE':
+        # Delete the method
+        _('Preference.msg.confirmDelete')
+        pref.delete()
+    if pref.parent:
+        return redirect('preference', user=user.username, prefId=pref.parent.id)
+    else:
+        return redirect('preferences', user=user.username)
+        
