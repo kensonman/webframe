810f94626fe7d3d712d2be4be2b2eca3d5ed58f1
Kenson Man <kenson@breakthrough.org.hk>
2017-08-09 11:44:35 +0800

    Add the mindate and maxdate jquery.validation rules

--- static/js/jqueryvalidation.dateformat.js
+++
@@ -1,5 +0,0 @@
-jQuery.validator.addMethod('dateformat', function(val, element, params){
-	if(typeof(params)=='undefined')
-		params='YYYY-MM-DD';
-	return this.optional(element) || moment(val, params, true).isValid();
-}, 'Please enter the value in valid date-format');
