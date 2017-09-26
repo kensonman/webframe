0da4aa6728fae017929d4beaa6b330b16566f04f
Kenson Man (MacbookAir) <kenson.idv.hk@gmail.com>
2017-09-26 23:43:43 +0800

    Add the ajax-users

--- urls.py
+++ urls.py
@@ -6,10 +6,13 @@ from django.conf import settings
 urlpatterns=[
    url(r'^login/?$', views.login, name='login'),
    url(r'^logout/?$', views.logout, name='logout'),
+
    url(r'^users/?$', views.users, name='users'),
    url(r'^users/(?P<user>[^/]*)/?$', views.user, name='user'),
    url(r'^users/(?P<user>[^/]*)/prefs/?$', views.prefs, name='prefs'),
    url(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/?$', views.pref, name='pref'),
+
+   url(r'^ajax/users/?$', views.ajaxUsers, name='ajax-users'),
 
    url(r'^jsi18n/webframe/$', javascript_catalog, {'packages':('webframe',),'domain':'django'}, name='js'),
 ] 
