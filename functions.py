98f57e077974a64adc038376b02d71db362b5ec7
Kenson Man <kenson@breakthrough.org.hk>
2017-11-30 15:59:01 +0800

    Fixing the import error

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.http import HttpRequest
 from django.utils import timezone
 from netaddr import IPAddress, IPNetwork
-from ddeprecation import deprecated
+from deprecation import deprecated
 import os, logging, pytz
 
 logger=logging.getLogger('webframe.functions')
