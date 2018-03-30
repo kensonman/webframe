5a6fc596b83c5958e2f658b4d75b0c650bc13e59
Kenson Man <kenson.idv.hk@gmail.com>
2018-03-30 15:54:26 +0800

    Fixing the javascript-catalog to django instead of djangojs

--- urls.py
+++ urls.py
@@ -1,15 +1,20 @@
-from django.conf.urls import url
-from . import views
+# -*- coding: utf-8 -*-
+#
 from django.conf import settings
+from django.urls import path, re_path, include
+from django.views.i18n import JavaScriptCatalog
+from . import views
 
 urlpatterns=[
-   url(r'^login/?$', views.login, name='login'),
-   url(r'^logout/?$', views.logout, name='logout'),
+   re_path(r'^login/?$', views.login, name='login'),
+   re_path(r'^logout/?$', views.logout, name='logout'),
 
-   url(r'^users/?$', views.users, name='users'),
-   url(r'^users/(?P<user>[^/]*)/?$', views.user, name='user'),
-   url(r'^users/(?P<user>[^/]*)/prefs/?$', views.prefs, name='prefs'),
-   url(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/?$', views.pref, name='pref'),
+   re_path(r'^users/?$', views.users, name='users'),
+   re_path(r'^users/(?P<user>[^/]*)/?$', views.user, name='user'),
+   re_path(r'^users/(?P<user>[^/]*)/prefs/?$', views.prefs, name='prefs'),
+   re_path(r'^users/(?P<user>[^/]*)/prefs/(?P<prefId>[^/]*)/?$', views.pref, name='pref'),
 
-   url(r'^ajax/users/?$', views.ajaxUsers, name='ajax-users'),
+   re_path(r'^ajax/users/?$', views.ajaxUsers, name='ajax-users'),
+
+   re_path('jsi18n/', JavaScriptCatalog.as_view(domain='django', packages=['webframe',]), name='js'),
 ] 
