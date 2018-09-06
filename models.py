2df214677ca5447b5081320decfc044808120792
Kenson Man <kenson@breakthrough.org.hk>
2018-09-06 17:06:34 +0800

    Alter the preference name

--- models.py
+++ models.py
@@ -385,10 +385,10 @@ class Preference(ValueObject):
    objects             = PrefManager()
 
    def __str__(self):
-      return '(None)' if self.value is None else str(self.value)
+      return '(None)' if self.value is None else "{0}::{1}".format(self.name, self.value)
 
    def __unicode__(self):
-      return '(None)' if self.value is None else unicode(self.value)
+      return '(None)' if self.value is None else "{0}::{1}".format(self.name, self.value)
 
    def childs(self):
       return Preference.objects.filter(parent=self).order_by('sequence')
