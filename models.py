8cdfc870d3a3707ac378e409de4a882b5847d8b5
Kenson Man <kenson.idv.hk@gmail.com>
2019-04-19 12:38:57 +0800

    Provide the pagination in table templates

--- models.py
+++ models.py
@@ -361,7 +361,7 @@ class PrefManager(models.Manager):
          rst=rst[0]
       else:
          rst=Preference(name=name, value=defval)
-      if str(kwargs.get('returnValue', 'False')).upper() in ['TRUE', 'T', 'YES', 'Y', '1', 'ON']:
+      if str(kwargs.get('returnValue', 'True')).upper() in ['TRUE', 'T', 'YES', 'Y', '1', 'ON']:
          return rst.value
       return rst
      except:
