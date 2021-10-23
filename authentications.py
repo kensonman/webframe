252ce233b86eb720a928ae36f528f660a43b22cd
Kenson Man <kenson@kenson.idv.hk>
2021-10-23 09:52:33 +0100

    Support the Authentication case-sensitive/insensitive.

--- authentications.py
+++ authentications.py
@@ -6,6 +6,7 @@
 from datetime import datetime
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import get_user_model as usermodel
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import *
@@ -21,14 +22,34 @@ import logging
 logger=logging.getLogger('webframe.authentications')
 
 class AuthenticationBackend(ModelBackend):
+   '''
+   The authentication backend that including the effective period checking. Due to the default authentication backend (Django provided) does not include the effective period checking. 
+
+   This authentication backend will also **ignore** the case of username.
+
+   USAGE
+   ====
+
+   ```Python
+   # proj/settings.py
+   AUTHENTICATION_BACKENDS = [
+      'webframe.authentications.AuthenticationBackend'
+   ]
+   AUTHENTICATION_BACKEND_CASE_INSENSITIVE = True #Set to Fase if you want case-sensitive
+   ```
+   '''
    def __init__(self):
       pass
    
    def authenticate(self, req, username=None, password=None):
       '''Handling the authentication'''
-      user=super().authenticate(req, username=username, password=password)
       messages.get_messages(req).used=True
       try:
+         if getattr(settings, 'AUTHENTICATION_BACKEND_CASE_INSENSITIVE', True):
+            logger.warning('B4 case-insensitive: {0}'.format(username))
+            username=usermodel()._default_manager.get(username__iexact=username).get_username()
+            logger.warning('AF case-insensitive: {0}'.format(username))
+         user=super().authenticate(req, username=username, password=password)
          if user:
             if user.is_superuser and user.is_active: 
                logger.debug('Ignore effective period checking for active superuser: {0}'.format(user.username))
@@ -53,9 +74,11 @@ class AuthenticationBackend(ModelBackend):
             #TODO: fire the signal here if necessary
             return user
          else:
-            logger.debug('Invalid username/password, {0}/{1}'.format(username, password))
-            messages.info(req, _('invalid username or password'))
-            return None
+            raise User.DoesNotExist
+      except User.DoesNotExist:
+         logger.debug('Invalid username/password, {0}/{1}'.format(username, password))
+         messages.info(req, _('invalid username or password'))
+         return None
       except:
          logger.exception('Unexpected exception when authenticate the user: {0}'.format(username))
 
